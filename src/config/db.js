ed20e3360f169367ec6bdca734eb9cdc60db0a07
3yaa <3yaa@users.noreply.github.com>
2025-11-10 22:54:22 -0800

    db setup changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,8 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: process.env.DB_CA_CERT,
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: false // connection will be local so no need to encryption
 });
