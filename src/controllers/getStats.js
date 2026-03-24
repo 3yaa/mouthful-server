80af68245ed266780308d88747215670e68b5642
3yaa <93301444+3yaa@users.noreply.github.com>
2026-03-23 17:18:31 -0700

    avg score

--- src/controllers/getStats.js
+++ src/controllers/getStats.js
@@ -4,8 +4,9 @@ export const getStats = async (req, res) => {
   try {
     const userId = req.user.id;
 
-    const result = await pool.query(
-      `
+    const [result, avgResult] = await Promise.all([
+      pool.query(
+        `
 			SELECT 'movies' AS media, status, COUNT(*) AS count 
 				FROM movies WHERE user_id=$1 GROUP BY status
 			UNION ALL
@@ -18,15 +19,40 @@ export const getStats = async (req, res) => {
 			SELECT 'games', status, COUNT(*) 
 				FROM games WHERE user_id=$1 GROUP BY status
 			`,
-      [userId],
-    );
+        [userId],
+      ),
+      pool.query(
+        `
+				SELECT 'movies' AS media, ROUND(AVG(score), 1) AS avg_score
+					FROM movies WHERE user_id=$1 AND score IS NOT NULL
+				UNION ALL
+				SELECT 'books', ROUND(AVG(score), 1)
+					FROM books WHERE user_id=$1 AND score IS NOT NULL
+				UNION ALL
+				SELECT 'shows', ROUND(AVG(score), 1)
+					FROM shows WHERE user_id=$1 AND score IS NOT NULL
+				UNION ALL
+				SELECT 'games', ROUND(AVG(score), 1)
+					FROM games WHERE user_id=$1 AND score IS NOT NULL
+			`,
+        [userId],
+      ),
+    ]);
 
+    // num items
     const stats = {};
     for (const row of result.rows) {
       if (stats[row.media] == null) {
         stats[row.media] = {};
       }
       stats[row.media][row.status] = Number(row.count);
+    }
+
+    // avg score
+    for (const row of avgResult.rows) {
+      if (stats[row.media]) {
+        stats[row.media].avgScore = Number(row.avg_score);
+      }
     }
 
     res.json({
